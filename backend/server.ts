fe522884381bf858baa3241bf07056bbfe3e335f
caidensanders-wsu <caiden.sanders@wsu.edu>
2024-11-16 23:45:22 -0800

    added prisma ORM, basic example so far

--- backend/server.ts
+++ backend/server.ts
@@ -5,14 +5,20 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+// Express Imports
 import express from 'express';
 import type { Request, Response } from 'express';
+
+// Prisma Imports
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const port = 3001;
 
+const prisma = new PrismaClient()
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World!');
+    res.send("Hello, World!");
 });
 
 app.listen(port, () => {
