a16312544a42f9dcd1db86572ca90cadde6d5dba
caidensanders-wsu <caiden.sanders@wsu.edu>
2024-11-17 15:17:22 -0800

    added prismaClient.ts as utility to initialize prisma client

---
+++ backend/src/utils/prismaClient.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) CPTS 322 Harry's Diner Project
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// Prisma Imports
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default prisma;
