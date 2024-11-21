cf9eeb8250ee777f13898e90dea5fa513b18a6c1
Nicholas Bellinger <bellinger.nicholasj@icloud.com>
2024-11-21 03:21:41 -0800

    Fix

--- src/Launchpad.tsx
+++ src/Launchpad.tsx
@@ -1,14 +1,13 @@
 // src/LaunchPad.tsx
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './style.css'
 
 const LaunchPad: React.FC = () => { // Renamed component
   const navigate = useNavigate();
 
   return (
     <div id='wrapper'>
-      <button id='return-button' onClick={() => navigate('/login')}>Log In / Sign Up</button>
+      <button id='return-button' onClick={() => navigate('/Login')}>Log in / Sign up</button>
       <div id="component">
         <h2 className="welcome-text">Welcome to the Restaurant App</h2>
         <div id='button-grid'>
