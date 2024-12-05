cac1df6fe0d96584780b8a0290381ceb7504f0e6
Nicholas Bellinger <bellinger.nicholasj@icloud.com>
2024-12-05 11:09:18 -0800

    Update LaunchPad.tsx

--- src/LaunchPad.tsx
+++ src/LaunchPad.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from './AuthContext';
 import './style.css';
 
 const LaunchPad: React.FC = () => {
   const navigate = useNavigate();
+  const { role } = useAuth();
 
   return (
     <div id="wrapper">
-      <button id="return-button" onClick={() => navigate('/login')}>
+      <button id="login-button" onClick={() => navigate('/login')}>
         Log In / Sign Up
       </button>
       <div id="component">
-        <h2 className="welcome-text">Welcome to the Restaurant App</h2>
+        <h2 className="welcome-text">Welcome to the Harry's Diner!</h2>
         <div id="button-grid">
           <button id="button-grid-button" onClick={() => navigate('/menu')}>
             Menu
           </button>
-          <button id="button-grid-button" onClick={() => navigate('/inventory')}>
-            Inventory
-          </button>
+          {role === 'admin' && (
+            <button id="button-grid-button" onClick={() => navigate('/inventory')}>
+              Inventory
+            </button>
+          )}
+
           <button id="button-grid-button" onClick={() => navigate('/order-notification')}>
             See Order Notification
-          </button>
-          <button id="button-grid-button" onClick={() => navigate('/modify-order-notification')}>
-            Modify Order Notification
           </button>
         </div>
       </div>
