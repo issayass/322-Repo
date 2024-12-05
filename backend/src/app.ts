6e71f3c244609596041a385535672bd41d5cab3c
caidensanders-wsu <caiden.sanders@wsu.edu>
2024-12-05 13:55:23 -0800

    got rid of arbitrary CORS rules.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -23,15 +23,8 @@ const app = express();
 
 app.use(express.json());
 
-const corsOptions = {
-    origin: ['http://localhost:5173', 'https://effulgent-longma-fd621a.netlify.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-}
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use(cors());
+app.options('*', cors());
 
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
