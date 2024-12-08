0230f1c2f4f42f9d06d5472208a23b8e0228d296
issayass <145182637+issayass@users.noreply.github.com>
2024-12-08 15:49:34 -0800

    Update axiosInstance.tsx

--- frontend/src/axiosInstance.tsx
+++ frontend/src/axiosInstance.tsx
@@ -1,24 +1,22 @@
-
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://harrysdiner.hopto.org/',
+  baseURL: 'http://3.145.154.168:3002', 
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = Cookies.get('authToken');
-    if (token && config.headers) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+// Optional: Add an interceptor to attach the token if stored in cookies
+axiosInstance.interceptors.request.use((config) => {
+  const token = Cookies.get('authToken');
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
 
 export default axiosInstance;
